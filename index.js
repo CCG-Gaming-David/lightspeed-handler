f20f0c3eed011940ccfd54c6ebc7debb717e6e8e
CCG-Gaming-David <david@collectiblescardsandgames.com>
2026-07-08 22:40:48 +0000

    Webhook setup GitHub.

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/webhook', express.json({type: 'application/json'}), (request, response) => {
+
+  // Respond to indicate that the delivery was successfully received.
+  // Your server should respond with a 2XX response within 10 seconds of receiving a webhook delivery. If your server takes longer than that to respond, then GitHub terminates the connection and considers the delivery a failure.
+  response.status(202).send('Accepted');
+  console.log("Webhook Found!");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
