6cb2ab28750846b1c4319003a4d2ee0f40c67ce9
CCG-Gaming-David <david@collectiblescardsandgames.com>
2026-07-13 05:26:08 +0000

    urlencoded to true

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 var jsonParser = bodyParser.json()
  
 // create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: true })
  
 
 app.post('/webhook', async (req, res) => {
