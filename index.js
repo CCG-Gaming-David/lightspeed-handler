3a0ae4a3de02dd02ae419543dcf648c29b7934d4
CCG-Gaming-David <david@collectiblescardsandgames.com>
2026-07-13 05:31:29 +0000

    Removed cors

--- index.js
+++ index.js
@@ -4,14 +4,6 @@ const bodyParser = require("body-parser");
 
 
 
-const cors = require('cors');
-const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
-
 
 app.use(express.json());
 app.use(bodyParser.json());
